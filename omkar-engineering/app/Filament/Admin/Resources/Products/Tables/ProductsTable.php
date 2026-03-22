08ef4ef755d36bd0d7daad21f4d49a66c63dd499
gns-raval9577 <111429148+gns-raval9577@users.noreply.github.com>
2026-03-22 12:51:37 +0530

    kanubhai changes

--- omkar-engineering/app/Filament/Admin/Resources/Products/Tables/ProductsTable.php
+++ omkar-engineering/app/Filament/Admin/Resources/Products/Tables/ProductsTable.php
@@ -2,9 +2,20 @@
 
 namespace App\Filament\Admin\Resources\Products\Tables;
 
-use Filament\Actions\BulkActionGroup;
+use App\Models\Product;
+use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Notifications\Notification;
+use Filament\Support\Enums\FontWeight;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Table;
 
 class ProductsTable
@@ -13,18 +24,29 @@ class ProductsTable
     {
         return $table
             ->columns([
-                //
-            ])
-            ->filters([
-                //
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable()
+                    ->weight(FontWeight::Medium),
+                TextColumn::make('description')
+                    ->searchable()
+                    ->sortable(),
+                ImageColumn::make('image')
+                    ->circular()
+                    ->size(48),
+                TextColumn::make('updated_at')
+                    ->label('Last modified at')
+                    ->date(),
             ])
             ->recordActions([
-                EditAction::make(),
-            ])
-            ->toolbarActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make(),
+                ActionGroup::make([
+                    ViewAction::make(),
+                    EditAction::make(),
+                    DeleteAction::make(),
                 ]),
+            ])
+            ->groupedBulkActions([
+                DeleteBulkAction::make()
             ]);
     }
 }
