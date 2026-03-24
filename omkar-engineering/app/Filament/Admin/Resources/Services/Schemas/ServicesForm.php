a767943c19264a1b4bb9a42b28d3b7bbbcc559d8
kanupatel <kanupatel1989@gmail.com>
2026-03-24 23:01:19 +0530

    kanu-work
    commit

--- omkar-engineering/app/Filament/Admin/Resources/Services/Schemas/ServicesForm.php
+++ omkar-engineering/app/Filament/Admin/Resources/Services/Schemas/ServicesForm.php
@@ -3,6 +3,12 @@
 namespace App\Filament\Admin\Resources\Services\Schemas;
 
 use Filament\Schemas\Schema;
+use App\Models\Services;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\FileUpload;
+use Filament\Schemas\Components\Utilities\Set;
+use Illuminate\Support\Str;
 
 class ServicesForm
 {
@@ -10,7 +16,37 @@ class ServicesForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('title')
+                    ->required()
+                    ->maxLength(255)
+                    ->live(onBlur: true)
+                    ->afterStateUpdated(fn (string $operation, $state, Set $set) => $operation === 'create' ? $set('slug', Str::slug($state)) : null),
+
+                TextInput::make('slug')
+                    ->disabled()
+                    ->dehydrated()
+                    ->required()
+                    ->maxLength(255)
+                    ->unique(Services::class, 'slug', ignoreRecord: true),
+
+                TextInput::make('description')
+                    ->required()
+                    ->maxLength(255),
+
+                TextInput::make('sort_description')
+                    ->required()
+                    ->maxLength(255),
+
+                FileUpload::make('image')
+                    ->label('Service Image')
+                    ->image() // only image
+                    ->directory('services-image') // storage/app/public/services-icons
+                    ->imagePreviewHeight('100')
+                    ->maxSize(1024), // 1MB
+
+                // Toggle::make('status')
+                //     ->default(true),
             ]);
     }
 }
+
--- omkar-engineering/app/Filament/Admin/Resources/Services/Tables/ServicesTable.php
+++ omkar-engineering/app/Filament/Admin/Resources/Services/Tables/ServicesTable.php
@@ -2,34 +2,58 @@
 
 namespace App\Filament\Admin\Resources\Services\Tables;
 
-use Filament\Actions\BulkActionGroup;
+use App\Models\Services;
+use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Actions\ForceDeleteBulkAction;
-use Filament\Actions\RestoreBulkAction;
-use Filament\Tables\Filters\TrashedFilter;
+use Filament\Actions\ViewAction;
+use Filament\Notifications\Notification;
+use Filament\Support\Enums\FontWeight;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Table;
 
-class ServicesTable
+class servicesTable
 {
     public static function configure(Table $table): Table
     {
         return $table
             ->columns([
-                //
-            ])
-            ->filters([
-                TrashedFilter::make(),
+                TextColumn::make('title')
+                    ->searchable()
+                    ->sortable()
+                    ->weight(FontWeight::Medium),
+                TextColumn::make('slug')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('description')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('sort_description')
+                    ->searchable()
+                    ->sortable(),
+                ImageColumn::make('image')
+                    ->circular()
+                    ->size(48),
+                TextColumn::make('updated_at')
+                    ->label('Last modified at')
+                    ->date(),
             ])
             ->recordActions([
-                EditAction::make(),
-            ])
-            ->toolbarActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make(),
-                    ForceDeleteBulkAction::make(),
-                    RestoreBulkAction::make(),
+                ActionGroup::make([
+                    ViewAction::make(),
+                    EditAction::make(),
+                    DeleteAction::make(),
                 ]),
+            ])
+            ->groupedBulkActions([
+                DeleteBulkAction::make()
             ]);
     }
 }
+
